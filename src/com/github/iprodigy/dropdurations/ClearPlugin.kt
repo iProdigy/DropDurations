94cc96b544e54ffdb5653a379af1d556292c1f65
iProdigy <prodigycreep@gmail.com>
2016-10-28 22:05:37 -0500

    Kotlin base

---
+++ src/com/github/iprodigy/dropdurations/ClearPlugin.kt
@@ -0,0 +1,15 @@
+package com.github.iprodigy.dropdurations
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ClearPlugin() : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
